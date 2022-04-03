75ce767a833c70e0d5e4afd2210c7d510ad41f2e
iiYese <iiyese@outlook.com>
2022-04-03 13:41:14 +0100

    changed to dioxus

--- posts/my-first-post.md
+++
@@ -1,6 +0,0 @@
----
-title: "My First Post"
-date: 2021-10-04T11:11:18+01:00
-draft: true
----
-
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use dioxus::prelude::*;
+
+fn main() {
+    dioxus::web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx!{
+        div { "hello, wasm!" }
+    })
+}
+
